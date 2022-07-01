87b6f2f7079a11a361494010fcaaba8c7d26b266
LucasL72 <lucasj.ledoux@gmail.com>
2022-07-01 18:46:09 +0200

    axios

--- my-app/src/config/axios.js
+++ my-app/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://grainedecitoyenmontgesnois.fr/api" /*"https://localhost:3030/api",*/,
+  baseURL: "http://localhost:3030/api",
   headers: { "X-Custom-Header": "Site GDC" },
   timeout: 2000,
 });
