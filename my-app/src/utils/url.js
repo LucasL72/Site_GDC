9ddf8d044b7a72f3e83717cda3363ac210a249e9
LucasL72 <lucasj.ledoux@gmail.com>
2022-07-02 16:16:43 +0200

    update urls

--- my-app/src/utils/url.js
+++ my-app/src/utils/url.js
@@ -1,7 +1,5 @@
-const urlImgArt= "http://localhost:3030/api/assets/Images/Articles/"
-const urlImgAlbum= "http://localhost:3030/api/assets/Images/Album/"
-const urlImgUsers= "http://localhost:3030/api/assets/Images/Users/"
-
-
+const urlImgArt= "https://grainedecitoyenmontgesnois.fr/api/assets/Images/Articles/"
+const urlImgAlbum= "https://grainedecitoyenmontgesnois.fr/api/assets/Images/Album/"
+const urlImgUsers= "https://grainedecitoyenmontgesnois.fr/api/assets/Images/Users/"
 
 module.exports = {urlImgArt,urlImgAlbum,urlImgUsers}
